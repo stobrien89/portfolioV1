6b32437e41bf3dee2fb67d066945f59004b6e2af
stobrien89 <stobrien@crimson.ua.edu>
2020-07-18 10:51:43 -0700

    added drawer for mobile nav

--- portfolio_flutter_web/lib/main.dart
+++ portfolio_flutter_web/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_flutter_web/header/header_view.dart';
 import 'package:portfolio_flutter_web/nav_bar/nav_bar_view.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,6 +27,7 @@ class PortfolioView extends StatelessWidget {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
+      drawer: DrawerView(),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -35,6 +37,49 @@ class PortfolioView extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class DrawerView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveBuilder(
+      builder: (_, size) {
+        if (!size.isMobile) return SizedBox();
+        return Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
